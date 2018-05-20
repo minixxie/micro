e3bac59af56c1e8ba0a030c240cb2b549b4ef6f0
Hyper <hyper.jiang@lalamove.com>
2018-05-20 12:13:16 +0800

    UPT: optimise code

--- micro.go
+++ micro.go
@@ -73,7 +73,7 @@ func (s *Service) Start(httpPort uint16, grpcPort uint16, reverseProxyFunc Rever
 	go func() {
 		// Start HTTP/1.0 server at :80
 		if err := grpcGateway(grpcPort, httpPort, reverseProxyFunc); err != nil {
-			log.Fatalf("failed to serve: %v", err)
+			log.Fatalf("failed to start gRPC gateway: %v", err)
 		}
 	}()
 
