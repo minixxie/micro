099ac5ab6edd7875689e30f4aed242d6ecc739cf
Hyper <hyper.jiang@lalamove.com>
2018-05-21 12:49:46 +0800

    FIX: do not use channel as it will block the process

--- micro_test.go
+++ micro_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"testing"
 	"time"
@@ -13,12 +14,11 @@ import (
 	"google.golang.org/grpc"
 )
 
-func TestNewService(t *testing.T) {
-	s := NewService(
-		[]grpc.StreamServerInterceptor{},
-		[]grpc.UnaryServerInterceptor{},
-	)
-	reverseProxyFunc := func(
+var reverseProxyFunc ReverseProxyFunc
+var httpPort, grpcPort uint16
+
+func init() {
+	reverseProxyFunc = func(
 		ctx context.Context,
 		mux *runtime.ServeMux,
 		grpcHostAndPort string,
@@ -27,11 +27,18 @@ func TestNewService(t *testing.T) {
 		return nil
 	}
 
-	SwaggerFile = "./swagger_demo.json"
-
-	var httpPort, grpcPort uint16
 	httpPort = 8888
 	grpcPort = 9999
+
+	SwaggerFile = "./swagger_demo.json"
+}
+
+func TestNewService(t *testing.T) {
+	s := NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+	)
+
 	go func() {
 		if err := s.Start(httpPort, grpcPort, reverseProxyFunc); err != nil {
 			t.Errorf("failed to serve: %v", err)
@@ -41,16 +48,41 @@ func TestNewService(t *testing.T) {
 	// wait 1 second for the server start
 	time.Sleep(1 * time.Second)
 
+	// check if the http server is up
+	httpHost := fmt.Sprintf(":%d", httpPort)
+	_, err := net.Listen("tcp", httpHost)
+	assert.Error(t, err)
+
+	// check if the grpc server is up
+	grpcHost := fmt.Sprintf(":%d", grpcPort)
+	_, err = net.Listen("tcp", grpcHost)
+	assert.Error(t, err)
+
+	// check if the http endpoint works
 	client := &http.Client{}
 	resp, err := client.Get(fmt.Sprintf("http://127.0.0.1:%d/swagger.json", httpPort))
 	if err != nil {
 		t.Error(err)
 	}
 	assert.Equal(t, 200, resp.StatusCode)
+}
 
-	// listen tcp :8888 already in use
-	err = s.Start(httpPort, grpcPort, reverseProxyFunc)
+func TestPortsUnavailable(t *testing.T) {
+	s := NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+	)
+
+	// ports 8888 and 9999 already in use
+	err := s.Start(httpPort, grpcPort, reverseProxyFunc)
 	assert.Error(t, err)
+}
+
+func TestErrorReverseProxyFunc(t *testing.T) {
+	s := NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+	)
 
 	// mock error from reverseProxyFunc
 	errText := "reverse proxy func error"
@@ -62,6 +94,7 @@ func TestNewService(t *testing.T) {
 	) error {
 		return errors.New(errText)
 	}
-	err = s.Start(httpPort, grpcPort, reverseProxyFunc)
-	assert.EqualError(t, err, errText)
+
+	err := s.Start(httpPort, grpcPort, reverseProxyFunc)
+	assert.Error(t, err)
 }
