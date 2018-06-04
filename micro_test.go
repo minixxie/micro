e971ce796a31d6a631478840e9c0929d3a852219
Hyper <hyper.jiang@lalamove.com>
2018-06-04 15:12:27 +0800

    ADD: upRedoc option & Stop function

--- micro_test.go
+++ micro_test.go
@@ -37,6 +37,7 @@ func TestNewService(t *testing.T) {
 	s := NewService(
 		[]grpc.StreamServerInterceptor{},
 		[]grpc.UnaryServerInterceptor{},
+		true,
 	)
 
 	go func() {
@@ -71,19 +72,55 @@ func TestNewService(t *testing.T) {
 		t.Error(err)
 	}
 	assert.Equal(t, 200, resp.StatusCode)
-}
 
-func TestPortsUnavailable(t *testing.T) {
+	// another service
+	s2 := NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+		false,
+	)
+
 	// http port 8888 already in use
-	err := grpcGateway(httpPort, grpcPort, reverseProxyFunc)
+	err = s2.startGrpcGateway(httpPort, grpcPort, reverseProxyFunc)
 	assert.Error(t, err)
 
 	// grpc port 9999 alreday in use
-	grpcServer(grpc.NewServer(), grpcPort)
+	err = s2.startGrpcServer(grpcPort)
 	assert.Error(t, err)
+
+	// stop the first server
+	s.Stop()
+
+	// run a new service again
+	s = NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+		false,
+	)
+	go func() {
+		if err := s.Start(httpPort, grpcPort, reverseProxyFunc); err != nil {
+			t.Errorf("failed to serve: %v", err)
+		}
+	}()
+
+	// wait 1 second for the server start
+	time.Sleep(1 * time.Second)
+
+	// the redoc is not up for the second server
+	resp, err = client.Get(fmt.Sprintf("http://127.0.0.1:%d/docs", httpPort))
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, 404, resp.StatusCode)
 }
 
 func TestErrorReverseProxyFunc(t *testing.T) {
+	s := NewService(
+		[]grpc.StreamServerInterceptor{},
+		[]grpc.UnaryServerInterceptor{},
+		false,
+	)
+
 	// mock error from reverseProxyFunc
 	errText := "reverse proxy func error"
 	reverseProxyFunc = func(
@@ -95,6 +132,6 @@ func TestErrorReverseProxyFunc(t *testing.T) {
 		return errors.New(errText)
 	}
 
-	err := grpcGateway(httpPort, grpcPort, reverseProxyFunc)
+	err := s.startGrpcGateway(httpPort, grpcPort, reverseProxyFunc)
 	assert.EqualError(t, err, errText)
 }
